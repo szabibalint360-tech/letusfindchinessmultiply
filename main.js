10b205d3bedcfc4cff0237d802fcb9bf89c83723
smekker11 <mekkerdavid@gmail.com>
2025-12-05 23:34:07 +0200

    api function prototypes

--- main.js
+++ main.js
@@ -5,17 +5,6 @@ import { Locations,  } from './tables/locations.js';
 const app = express();
 const PORT = 8080;
 
-<<<<<<< HEAD
-
-app.get('/locations', async (req, res) => {
-    await Locations.bulkCreate([
-    { judeti: "CLUJ", locatie: "TURDA", numLoc: 47000 },
-    { judeti: "CLUJ", locatie: "DEJ", numLoc: 33000 },
-    { judeti: "BIHOR", locatie: "ORADEA", numLoc: 196367 }
-]);
- res.status(200).send("bombocla avem turda")
-})
-=======
 app.post('AddCity', AddCity);
 
 
@@ -23,6 +12,8 @@ app.post('AddRisk', AddRisk);
 
 
 app.use(express.json())
->>>>>>> 890675a (api function prototypes)
+
+
+
 
 app.listen(PORT,() => console.log(`test api on http://localhost:${PORT}`))
