dd98c06696bb24fdf3499638d5b2425566313648
smekker11 <mekkerdavid@gmail.com>
2025-12-06 00:34:32 +0200

    dotenv validation and table relationship updates

--- tables/risks.js
+++ tables/risks.js
@@ -1,22 +1,36 @@
 import { DataTypes, Sequelize } from "sequelize";
+import { Locations } from "./locations";
+require('dotenv').config();
+
+import { POSSIBLE_RISKS } from "../vld/risk-validation.js";
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: './db/risks.sqlite'
+    storage: './db/analytics.sqlite'
 
 });
 
-export const Risks = sequelize.define('Risks', {
+export var Risks = sequelize.define('Risks', {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
     },
+    typeofRisk: {
+        type: DataTypes.STRING,
+        validate: {
+            isIn: [POSSIBLE_RISKS]
+        }
+    },
     info: {
         type: DataTypes.STRING(2048)
     },
     satRel: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
+        references: {
+            model: Locations,
+            key: 'id'
+        }
     },
     arrayStringNumeFisiere: {
         type: DataTypes.JSON
@@ -25,6 +39,8 @@ export const Risks = sequelize.define('Risks', {
     tableName: 'risks',
     timestamps: true
 });
+
+Risks.belongsTo(Locations, { foreignKey: 'satRel' });
 
 sequelize.sync()
     .then(() => { console.log("DB connection working!"); })
