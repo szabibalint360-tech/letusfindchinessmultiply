29382da7838ca804a6eb330fee673ad461f6da36
szabi <szabibalint360@gmail.com>
2025-12-06 02:45:45 +0200

    commit for review

---
+++ vld/risk-validation.js
@@ -0,0 +1,10 @@
+export const POSSIBLE_RISKS = Object.freeze([
+    'Flood',
+    'Earthquake',
+    'Wildfire',
+    'Storm',
+    'Landslide',
+    'Drought',
+    'Pandemic_COV19_RESPONSE',
+    'Industrial Environment Accident'
+]);
